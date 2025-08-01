e6b83465a14d611f18f3dabbc192d11c7abd0395
Paulina Leal <lealherrerapaulina@gmail.com>
2025-08-01 16:48:24 -0600

    Completed all parts of assignment 3

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -28,38 +28,40 @@ def draw_cat_plot():
     df_cat = df_cat.rename("total").reset_index()
 
     # 7
-    #sns.catplot(data= df_cat, x =df_cat["variable"], y=df_cat["total"], row= 1, col=df["cardio"], kind= "bar")
-
+    sns.catplot(data= df_cat, x ="variable", y="total", col="cardio", kind= "bar", hue= "value", legend="brief")
     # 8
+    
     fig = sns.catplot(data= df_cat, x ="variable", y="total", col="cardio", kind= "bar", hue= "value", legend="brief")
     
     # 9
     fig.savefig('catplot.png')
     return fig
 
-
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
-
+    df_heat = df[(df["ap_lo"] <= df["ap_hi"]) & 
+                (df["height"] >= df["height"].quantile(0.025)) & 
+                (df["height"] <= df["height"].quantile(0.975)) &
+                (df["weight"] >= df["weight"].quantile(0.025)) &
+                (df["weight"] <= df["weight"].quantile(0.975))]
+    
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
+    mask = np.triu(np.ones_like(corr, dtype= bool))
 
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots()
 
     # 15
-
-
+    sns.heatmap(corr, annot = True, vmin=-0.1, vmax=0.3, fmt=".1f", linewidths=.5, linecolor = "white",mask=mask, center= 0, square = True, annot_kws={"size":6})
 
     # 16
     fig.savefig('heatmap.png')
     return fig
 
 draw_cat_plot()
+draw_heat_map()
